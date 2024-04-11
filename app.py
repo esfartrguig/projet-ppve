36a043fd3540200fc67463ebc82fe1c5bf4b54b3
Noah <nali1@francoisdesales.be>
2024-04-11 08:47:11 +0200

    version1.2

--- app.py
+++ app.py
@@ -6,10 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    email = request.form['email']
-    return render_template('result.html', email=email)
-
 if __name__ == '__main__':
     app.run(debug=True)
