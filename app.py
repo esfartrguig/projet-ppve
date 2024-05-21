c2af46d12b64be02f016287de690b541b7bf33eb
elyas <esfartrguig@francoisdesales.be>
2024-05-21 11:14:21 +0200

    modifiction de la route par défaut

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
+@app.route("/index", methods=['GET'])
 def index():
-    return render_template('index.html')
+    csv_list_ecole_file_path = "csv/liste_ecole_nv.csv"
+    csv_parc_auto_file_path = "csv/park_auto.csv"
+    vehicules = {}
+
+    # Lecture du fichier CSV contenant les informations sur les véhicules
+    with open(csv_parc_auto_file_path, 'r', newline='', encoding='utf-8') as csvfile:
+        vehicule_reader = csv.DictReader(csvfile, delimiter=';')
+        for item in vehicule_reader:
+            vehicules[item['immatriculation']] = item
+
+    # Lecture du fichier CSV contenant les informations sur les utilisateurs
+    with open(csv_list_ecole_file_path, 'r', newline='', encoding='utf-8') as csvfile:
+        utilisateur_reader = csv.DictReader(csvfile, delimiter=';')
+        vu_liste = []
+        for item in utilisateur_reader:
+            user_immatriculation = item['immatriculation']
+            if user_immatriculation in vehicules:
+                vehicule_info = vehicules[user_immatriculation]
+                # Création d'un dictionnaire contenant les détails de l'utilisateur et du véhicule utilisé
+                vu_dict = {
+                            "Nom utilisateur": item['nom'], 
+                            "Prénom utilisateur": item['prenom'],
+                            "Véhicule - Marque": vehicule_info['marque'],
+                            "Modèle": vehicule_info['modele'],
+                            "Plaque d'immatriculation": vehicule_info['immatriculation']
+                           }
+                vu_liste.append(vu_dict)
+   
+    return render_template('index.html', data=vu_liste)
 
 if __name__ == '__main__':
     app.run(debug=True)
