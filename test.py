921ab9e37eaf14dc3bdea5d09e7ef2af98acf728
Yvoussure <yvoussure@francoisdesales.be>
2024-06-06 09:42:01 +0200

    fusion commit user et commit vehicule

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for
 import csv
-from pprint import pprint
+import os
 
-# Cette fonction affiche les détails des véhicules à partir d'un fichier CSV
-def fn_display_vehicule(csv_file_path):
-    with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
-        vehicule_reader = csv.DictReader(csvfile, delimiter=';')
-        for item in vehicule_reader:
-            print(f"Type : {item['type']}, Matricule : {item['matricule']}, Prénom : {item['prenom']}, Nom : {item['nom']}, Date de naissance : {item['naissance']}")
+app = Flask(__name__)
 
-# Cette fonction affiche les détails des utilisateurs et des véhicules qu'ils utilisent en les comparant avec deux fichiers CSV
-def fn_display_veh_and_user(csv_parc_auto_file_path, csv_list_ecole_file_path):
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    csv_list_ecole_file_path = "csv/liste_ecole_nv.csv"
+    csv_parc_auto_file_path = "csv/park_auto.csv"
     vehicules = {}
 
     # Lecture du fichier CSV contenant les informations sur les véhicules
@@ -28,28 +27,35 @@ def fn_display_veh_and_user(csv_parc_auto_file_path, csv_list_ecole_file_path):
                 vehicule_info = vehicules[user_immatriculation]
                 # Création d'un dictionnaire contenant les détails de l'utilisateur et du véhicule utilisé
                 vu_dict = {
-                            "Nom utilisateur": item['nom'], 
-                            "Prénom utilisateur": item['prenom'],
-                            "Véhicule - Marque": vehicule_info['marque'],
-                            "Modèle": vehicule_info['modele'],
-                            "Plaque d'immatriculation": vehicule_info['immatriculation']
-                           }
+                    "nom": item['nom'],
+                    "prenom": item['prenom'],
+                    "marque": vehicule_info['marque'],
+                    "modele": vehicule_info['modele'],
+                    "plaque": vehicule_info['immatriculation']
+                }
                 vu_liste.append(vu_dict)
-    return vu_liste
+   
+    return render_template('index.html', data=vu_liste)
 
-# Cette fonction trie la liste des personnes par nom puis par prénom
-def tri_personnes(liste_personnes):
-    liste_personnes_triee = sorted(liste_personnes, key=lambda x: (x['Nom utilisateur'], x['Prénom utilisateur']))
-    return liste_personnes_triee
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        firstname = request.form['firstname']
+        street = request.form['type']
+        phone = request.form['city']
+        postal_code = request.form['adresse']
+        
+        with open('data.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([name, firstname, street, phone, postal_code])
 
-def filtrer_par_marque(liste, marque):
-    return [element for element in liste if element.get("Véhicule - Marque") == marque]
+        return redirect(url_for('success'))
+    return render_template('form.html')
 
-csv_list_ecole_file_path = "csv/liste_ecole_nv.csv"
-csv_parc_auto_file_path = "csv/park_auto.csv"
-resultat = fn_display_veh_and_user(csv_parc_auto_file_path, csv_list_ecole_file_path)
-resultat_trie = tri_personnes(resultat)
-marque_filtree = filtrer_par_marque(resultat_trie, "TOYOTA")
-print(marque_filtree)
-# Affichage des résultats triés
-pprint(marque_filtree)
+@app.route('/success')
+def success():
+    return "Les informations ont bien été encodées"
+
+if __name__ == '__main__':
+    app.run(debug=True)
